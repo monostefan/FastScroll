c7b56a5a3ccc463335627e8314625514906ff4f2
Stefan Reinhard <stefan@monostream.com>
2016-10-26 11:35:48 +0200

    Show and hide scroll handle

--- Code/FastScroller.cs
+++ Code/FastScroller.cs
@@ -12,15 +12,16 @@ namespace FastScroll
 {
     public class FastScroller : LinearLayout
     {
-        private static int BUBBLE_ANIMATION_DURATION = 200;
+        private static int ANIMATION_DURATION = 200;
         private static int TRACK_SNAP_RANGE = 5;
 
         private TextView bubble;
         private View handle;
         private RecyclerView recyclerView;
-        private MyScrollListener scrollListener;
+        private RecyclerScrollListener scrollListener;
         private int height;
-        private ObjectAnimator currentAnimator = null;
+        private ObjectAnimator currentBubbleAnimator = null;
+        private ObjectAnimator currentHandleAnimator = null;
 
         public FastScroller(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
         {
@@ -37,7 +38,7 @@ namespace FastScroll
 
         private void init(Context context)
         {
-            scrollListener = new MyScrollListener(this);
+            scrollListener = new RecyclerScrollListener(this);
 
             Orientation = Orientation.Horizontal;
             SetClipChildren(false);
@@ -47,6 +48,7 @@ namespace FastScroll
             bubble = FindViewById<TextView>(Resource.Id.fastscroller_bubble);
             handle = FindViewById<View>(Resource.Id.fastscroller_handle);
             bubble.Visibility = ViewStates.Invisible;
+            handle.Visibility = ViewStates.Invisible;
         }
 
         protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
@@ -63,10 +65,17 @@ namespace FastScroll
                 case MotionEventActions.Down:
                     if (e.GetX() < handle.GetX())
                         return false;
-                    if (currentAnimator != null)
-                        currentAnimator.Cancel();
+
+                    if (currentBubbleAnimator != null)
+                        currentBubbleAnimator.Cancel();
                     if (bubble.Visibility == ViewStates.Invisible)
                         showBubble();
+
+                    if (currentHandleAnimator != null)
+                        currentHandleAnimator.Cancel();
+                    if (handle.Visibility == ViewStates.Invisible)
+                        showHandle(animated: false);
+
                     handle.Selected = true;
                     setPosition(e.GetY());
                     setRecyclerViewPosition(e.GetY());
@@ -81,6 +90,7 @@ namespace FastScroll
                 case MotionEventActions.Cancel:
                     handle.Selected = false;
                     hideBubble();
+                    hideHandle();
                     return true;
             }
 
@@ -128,29 +138,62 @@ namespace FastScroll
             bubble.SetY(getValueInRange(0, height - bubbleHeight, (int)(y - bubbleHeight / 2)));
         }
 
+        private void showHandle(bool animated = true)
+        {
+            handle.Visibility = ViewStates.Visible;
+
+            if (animated)
+            {
+                if (currentHandleAnimator != null)
+                    currentHandleAnimator.Cancel();
+                currentHandleAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(handle, "alpha", 0f, 1f).SetDuration(ANIMATION_DURATION);
+                currentHandleAnimator.Start();
+            }
+            else
+            {
+                handle.Alpha = 1f;
+            }
+        }
+
+        private void hideHandle(bool animated = true)
+        {
+            if (animated)
+            {
+                if (currentHandleAnimator != null)
+                    currentHandleAnimator.Cancel();
+                currentHandleAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(handle, "alpha", 1f, 0f).SetDuration(ANIMATION_DURATION);
+                currentHandleAnimator.AddListener(new HandleAnimationEndListener(this));
+                currentHandleAnimator.Start();
+            }
+            else
+            {
+                handle.Alpha = 0f;
+            }
+        }
+
         private void showBubble()
         {
             bubble.Visibility = ViewStates.Visible;
-            if (currentAnimator != null)
-                currentAnimator.Cancel();
-            currentAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(bubble, "alpha", 0f, 1f).SetDuration(BUBBLE_ANIMATION_DURATION);
-            currentAnimator.Start();
+            if (currentBubbleAnimator != null)
+                currentBubbleAnimator.Cancel();
+            currentBubbleAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(bubble, "alpha", 0f, 1f).SetDuration(ANIMATION_DURATION);
+            currentBubbleAnimator.Start();
         }
 
         private void hideBubble()
         {
-            if (currentAnimator != null)
-                currentAnimator.Cancel();
-            currentAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(bubble, "alpha", 1f, 0f).SetDuration(BUBBLE_ANIMATION_DURATION);
-            currentAnimator.AddListener(new MyListener(this));
-            currentAnimator.Start();
+            if (currentBubbleAnimator != null)
+                currentBubbleAnimator.Cancel();
+            currentBubbleAnimator = (ObjectAnimator)ObjectAnimator.OfFloat(bubble, "alpha", 1f, 0f).SetDuration(ANIMATION_DURATION);
+            currentBubbleAnimator.AddListener(new BubbleAnimationEndListener(this));
+            currentBubbleAnimator.Start();
         }
 
-        internal class MyListener : AnimatorListenerAdapter
+        internal class BubbleAnimationEndListener : AnimatorListenerAdapter
         {
             private FastScroller scroll;
 
-            public MyListener(FastScroller scroller)
+            public BubbleAnimationEndListener(FastScroller scroller)
             {
                 this.scroll = scroller;
             }
@@ -159,23 +202,61 @@ namespace FastScroll
             {
                 base.OnAnimationEnd(animation);
                 scroll.bubble.Visibility = ViewStates.Invisible;
-                scroll.currentAnimator = null;
+                scroll.currentBubbleAnimator = null;
             }
             public override void OnAnimationCancel(Animator animation)
             {
                 base.OnAnimationCancel(animation);
                 scroll.bubble.Visibility = ViewStates.Invisible;
-                scroll.currentAnimator = null;
+                scroll.currentBubbleAnimator = null;
             }
         }
 
-        internal class MyScrollListener : RecyclerView.OnScrollListener
+        internal class HandleAnimationEndListener : AnimatorListenerAdapter
+        {
+            private FastScroller scroll;
+
+            public HandleAnimationEndListener(FastScroller scroller)
+            {
+                this.scroll = scroller;
+            }
+
+            public override void OnAnimationEnd(Animator animation)
+            {
+                base.OnAnimationEnd(animation);
+                scroll.handle.Visibility = ViewStates.Invisible;
+                scroll.currentHandleAnimator = null;
+            }
+            public override void OnAnimationCancel(Animator animation)
+            {
+                base.OnAnimationCancel(animation);
+                scroll.handle.Visibility = ViewStates.Invisible;
+                scroll.currentHandleAnimator = null;
+            }
+        }
+
+        internal class RecyclerScrollListener : RecyclerView.OnScrollListener
         {
             private readonly FastScroller scroll;
 
-            public MyScrollListener(FastScroller scroller)
+            public RecyclerScrollListener(FastScroller scroller)
             {
                 this.scroll = scroller;
+            }
+
+            public override void OnScrollStateChanged(RecyclerView recyclerView, int newState)
+            {
+                if (!scroll.handle.Selected)
+                {
+                    if (newState == RecyclerView.ScrollStateIdle)
+                    {
+                        scroll.hideHandle();
+                    }
+                    else if (newState == RecyclerView.ScrollStateDragging)
+                    {
+                        scroll.showHandle();
+                    }
+                }
             }
 
             public override void OnScrolled(RecyclerView recyclerView, int dx, int dy)
